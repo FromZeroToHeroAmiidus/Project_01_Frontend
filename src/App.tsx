8e5a1d9ede15cfcde8f37f5db007479bac51aee2
Amiidus <amiidus@pwnator.com>
2024-08-02 23:09:03 +0200

    Trello : U5eFwuMI

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,206 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+// Import necessary modules
+import React, { useState, useEffect } from "react";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
+import Signup from "./pages/signup/Signup";
+import HomePage from "./pages/homePage/HomePage";
+import Error404 from "./pages/errorPage/Error404";
 
-function App() {
-  const [count, setCount] = useState(0)
+import Navbar from "./components/navbar/Navbar";
+
+import ProfileSettingsPage from "./settings/settingsPage/SettingsPage";
+
+import authStore from "./stores/auth.store";
+
+import "./index.css";
+import AllAuctions from "./pages/auctions/AllAuctions";
+import BiddingAuctions from "./pages/auctions/BiddingAuctions";
+import MyAuctions from "./pages/auctions/MyAuctions";
+import WonAuctions from "./pages/auctions/WonAuctions";
+import AuctionItem from "./pages/auctions/AuctionItem";
+
+import AddAuction from "./pages/auctions/addAuction/AddAuction";
+
+import { observer } from "mobx-react";
+import RetailLoginForm from "./pages/login_and_register/RetailLoginForm";
+import RetailSignupForm from "./pages/login_and_register/RetailSignupForm";
+import RetailForgotPasswordForm from "./pages/login_and_register/RetailForgotPasswordForm";
+import RetailPasswordResetConfirmForm from "./pages/login_and_register/RetailPasswordResetConfirmForm";
+
+const App: React.FC = () => {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  // for title change when clicking navbar
+  //const [title, setTitle] = useState("Default Title");
+
+  const hideNavbarLogin = location.pathname === "/login";
+  const hideNavbarRegister = location.pathname === "/signup";
+  const hideForgotPassword = location.pathname === "/forgot-password";
+  const hideResetPasswordConfirm = location.pathname === "/reset-password";
+  //  const hideNavbarUserLandingPage = location.pathname === "/me/myauctions";
+
+  // add auction
+  const [modalState, setModalState] = useState<{ [key: string]: boolean }>({
+    addAuction: false,
+    profileSettings: false,
+    miniSettings: false,
+  });
+
+  const handleOpenModal = (modalName: string) => {
+    setModalState((prevState) => ({ ...prevState, [modalName]: true }));
+  };
+
+  const handleCloseModal = (modalName: string) => {
+    setModalState((prevState) => ({ ...prevState, [modalName]: false }));
+  };
+  // add auction
+
+  useEffect(() => {
+    // This effect will run only once when the component mounts
+    if (authStore.user) {
+      setIsLoggedIn(true);
+    } else {
+      setIsLoggedIn(false);
+      // authStore.signout();
+      // userIdStorage.clearUser();
+      // fullUserStore.signout();
+      // window.location.href = "/";
+      // window.location.pathname = "/";
+    }
+  }, []); // Empty dependency array means this effect runs once on mount
+
+  useEffect(() => {}, [isLoggedIn]);
+
+  const [activeTab, setActiveTab] = useState<
+    "auctions" | "profile" | "unclicked"
+  >("unclicked");
+
+  // isLoggedIn ? setActiveTab("unclicked") : setActiveTab("auctions");
 
   return (
-    <>
+    <Router>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        {!hideNavbarLogin &&
+          !hideNavbarRegister &&
+          !hideForgotPassword &&
+          !hideResetPasswordConfirm && (
+            // !hideNavbarUserLandingPage && (
+            <Navbar
+              activeTab={activeTab}
+              setActiveTab={setActiveTab}
+              onOpenModal={handleOpenModal} // Pass the modal open handler to Navbar
+            />
+          )}
+        <Routes>
+          <Route
+            path="/"
+            element={isLoggedIn ? <MyAuctions /> : <HomePage />}
+          />
+          <Route
+            path="/user/settings"
+            element={isLoggedIn ? <ProfileSettingsPage /> : <Navigate to="/" />}
+          />
+          <Route
+            path="/login"
+            element={isLoggedIn ? <Navigate to="/" /> : <RetailLoginForm />}
+          />{" "}
+          <Route
+            path="/forgot-password"
+            element={
+              isLoggedIn ? <Navigate to="/" /> : <RetailForgotPasswordForm />
+            }
+          />
+          <Route
+            path="/reset-password"
+            element={
+              isLoggedIn ? (
+                <Navigate to="/" />
+              ) : (
+                <RetailPasswordResetConfirmForm />
+              )
+            }
+          />
+          <Route
+            path="/signup"
+            element={
+              isLoggedIn ? (
+                <Navigate to="/me/myautions" />
+              ) : (
+                <RetailSignupForm />
+              )
+            }
+          />
+          <Route
+            path="/auctions"
+            element={isLoggedIn ? <AllAuctions /> : <Navigate to="/" />}
+          />
+          <Route
+            path="/me/bidding"
+            element={
+              isLoggedIn ? (
+                <>
+                  {/* <Navbar activeTab={"profile"} setActiveTab={setActiveTab} onOpenModal={handleOpenModal}/> */}
+                  <BiddingAuctions /> {/* <RetailSignup /> */}
+                </>
+              ) : (
+                <Signup />
+              )
+            }
+          />
+          <Route
+            path="/me/myauctions"
+            element={
+              isLoggedIn ? (
+                <>
+                  {/* <Navbar activeTab={"profile"} setActiveTab={setActiveTab} onOpenModal={handleOpenModal}/> */}
+                  <MyAuctions />
+                  {/* <RetailPasswordReset /> */}
+                </>
+              ) : (
+                <Signup />
+              )
+            }
+          />
+          <Route
+            path="/me/won"
+            element={
+              isLoggedIn ? (
+                <>
+                  {/* <Navbar activeTab={"profile"} setActiveTab={setActiveTab} onOpenModal={handleOpenModal}/> */}
+                  <WonAuctions /> {/* <RetailLogin /> */}
+                </>
+              ) : (
+                <Signup />
+              )
+            }
+          />
+          <Route
+            path="/auctions/:id/bid"
+            element={isLoggedIn ? <AuctionItem /> : <HomePage />}
+          />
+          <Route
+            path="/auctions/create"
+            element={
+              isLoggedIn ? (
+                <AddAuction
+                  isOpen={modalState.addAuction}
+                  onClose={() => handleCloseModal("addAuction")}
+                />
+              ) : (
+                <Signup />
+              )
+            }
+          />
+          <Route path="*" element={<Error404 />} />
+        </Routes>
+        <br />
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    </Router>
+  );
+};
 
-export default App
+export default observer(App);
